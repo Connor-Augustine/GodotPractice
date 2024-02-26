dabba2838d1b6f8bfac5dafacb5856595080a21c
Connor <connoraaugustine@gmail.com>
2024-02-26 14:54:08 -0600

    test 1

--- Icon.cs
+++ Icon.cs
@@ -1,13 +1,23 @@
 using Godot;
 using System;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
 
 public partial class Icon : Sprite2D
 {
 	int speed = 10;
+	int screenWidth;
+	int screenHeight;
 	bool direction;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		screenWidth = 1920;
+		screenHeight = 1080;
+		ProjectSettings.SetSetting("display/window/size/viewport_width",screenWidth);
+		ProjectSettings.SetSetting("display/window/size/viewport_height",screenHeight);
 		direction = true;
 	}
 
@@ -15,17 +25,19 @@ public partial class Icon : Sprite2D
 	public override void _Process(double delta)
 	{
 		var temp = Position;
-		if(direction) {
-			temp.X += speed;
-		} else {
+		if(Input.IsKeyPressed(Key.W)) {
+			temp.Y -= speed;
+		} 
+		if(Input.IsKeyPressed(Key.A)) {
 			temp.X -= speed;
-		}
-		if(temp.X > 1000 && direction) {
-			direction = !direction; 
-		}
-		if(temp.X < 0 && !direction) {
-			direction = !direction;
-		}
+		} 
+		if(Input.IsKeyPressed(Key.S)) {
+			temp.Y += speed;
+		} 
+		if(Input.IsKeyPressed(Key.D)) {
+			temp.X += speed;
+		} 
 		Position = temp;
+		System.Console.Write("" + screenHeight + "" + screenWidth);
 	}
 }
